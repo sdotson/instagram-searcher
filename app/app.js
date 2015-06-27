b338eba0eb82e99be0ce18afaf691bb8c35445fe
Stu <stuart@blueprintinteractive.com>
2015-06-27 10:10:24 -0400

    id and secret now in comments

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 (function(){
 
+	/*CLIENT ID	94f991d47b36477986f220d397b9a12b*/
+
+	/*CLIENT SECRET	2be13ae9006049368473689c7a2fd083*/
+
 	angular.module('app', ['ngAnimate'])
 
 		.controller('SearchController', SearchController);
@@ -7,7 +11,6 @@
 		function SearchController($scope) {
 			$scope.puppies = "whoa";
 		}
-
 
 
 })();
